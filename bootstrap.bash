f9f61e6b6116a1c3035c37ccd79a83eca9bd4850
xoh <xoh@users.noreply.github.com>
2024-11-08 14:16:25 +0100

    Update bootstrap.bash

--- bootstrap.bash
+++ bootstrap.bash
@@ -2,6 +2,7 @@ apt install -y ansible git
 
 ansible-galaxy collection install community.general
 
-read -p "bootstrap vault location (USB): " bootstrapvaultpath
+read -p "bootstrap vault location [/mnt/vault/bootstrap.yml]: " bootstrapvaultpath
+bootstrapvaultpath=${bootstrapvaultpath:-/mnt/vault/bootstrap.yml}
 
 ANSIBLE_ASK_VAULT_PASS=True ansible-pull -U https://github.com/xoh/ansible -e "bootstrap_vault=$bootstrapvaultpath" bootstrap.yml
